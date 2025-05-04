d773ba1edbb03b57ef672b8d7f66994d3cf883bc
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-05-04 22:29:31 +0200

    clean up

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -30,7 +30,7 @@ function VanDetail() {
                         {van.type}
                     </i>
                     <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p className="van-price"><span>R{van.price}</span>/day</p>
                     <p>{van.description}</p>
                     <button className="link-button">Rent this van</button>
                 </div>
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -43,7 +43,7 @@ function Vans()
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
-                    <p>${van.price}<span>/day</span></p>
+                    <p>R{van.price}<span>/day</span></p>
                 </div>
                 <i className={`van-type ${van.type} selected`}>{van.type}</i>
             </Link>
