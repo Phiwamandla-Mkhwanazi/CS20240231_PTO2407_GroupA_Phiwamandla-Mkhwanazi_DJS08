4305f44afcd6ddef9c23eae5c972fa761f3763e2
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-05-04 14:21:15 +0200

    Fxining the NavBar with a Layout Route

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,14 @@ import About from './pages/About';
 import Vans from './pages/Vans';
 import VanDetail from './pages/VanDetail';
 
+
 //Server
 import "../server";
 
 function App() {
   return (
     <BrowserRouter>
-         <header>
-          <Link to="/">#VANLIFE</Link>
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
-          </nav>
-        </header>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
