6bb3d22879765713495ceb97d184d1d883893c5b
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-05-04 12:58:25 +0200

    Route Params - part 1

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
 //Server
 import "../server";
 
@@ -35,6 +36,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans/>} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,6 @@
+
+function VanDetail() {
+    return <h1>Van detail page goes here</h1>
+}
+
+export default  VanDetail;
