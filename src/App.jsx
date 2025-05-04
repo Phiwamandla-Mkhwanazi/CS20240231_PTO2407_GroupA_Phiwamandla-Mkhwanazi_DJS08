ffaf209dd5ef75acb2371f51971d903bb2c95210
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-05-04 15:43:10 +0200

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           
+          
           <Route path="vans" element={<Vans/>} />
           <Route path="vans/:id" element={<VanDetail />} />
           
